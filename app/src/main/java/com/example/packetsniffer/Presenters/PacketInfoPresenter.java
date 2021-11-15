98b6c23d7d16a84c0f407a74977838c83ee45053
maennis <mark.andrew.ennis@gmail.com>
2021-11-15 07:51:32 -0800

    Improved TCP packet display

--- app/src/main/java/com/example/packetsniffer/Presenters/PacketInfoPresenter.java
+++ app/src/main/java/com/example/packetsniffer/Presenters/PacketInfoPresenter.java
@@ -8,12 +8,16 @@ import com.example.packetsniffer.Models.PcapRepository;
 import java.io.IOException;
 
 import io.pkts.packet.TCPPacket;
+import io.pkts.packet.TransportPacket;
 import io.pkts.packet.UDPPacket;
 import io.pkts.protocol.Protocol;
 
 public class PacketInfoPresenter {
 
     private static final String TAG = PacketInfoPresenter.class.getName();
+
+    private static final String TCP = "TCP";
+    private static final String UDP = "UDP";
 
     private PcapRepository pcapRepository;
     private PcapEntry entry;
@@ -27,16 +31,26 @@ public class PacketInfoPresenter {
 
     public void displayPacket() {
         try {
-            if (entry.getProtocol() == "TCP") {
-                // Display TCP specific info
-                TCPPacket packet = (TCPPacket) entry.getPacket().getPacket(Protocol.TCP);
-                view.displaySrc(packet.getSourceIP());
-                view.displayDst(packet.getDestinationIP());
-            } else if (entry.getProtocol() == "UDP") {
-                // Display UDP specific info
-                UDPPacket packet = (UDPPacket) entry.getPacket().getPacket(Protocol.UDP);
-                view.displaySrc(packet.getSourceIP());
-                view.displayDst(packet.getDestinationIP());
+            view.displayArrivalTime(new Long(entry.getArrivalTime()).toString());
+                if (entry.getProtocol() == TCP) {
+                    // Display TCP specific info
+                    TCPPacket packet = (TCPPacket) entry.getPacket().getPacket(Protocol.TCP);
+                    view.displayProtocol(TCP);
+                    view.displayTcp(new Long(packet.getAcknowledgementNumber()).toString(),
+                            new Long(packet.getSequenceNumber()).toString(),
+                            displayFlags(packet));
+                    view.displaySrc(packet.getSourceIP());
+                    view.displayDst(packet.getDestinationIP());
+                    view.displaySrcPort(new Integer(packet.getSourcePort()).toString());
+                    view.displayDstPort(new Integer(packet.getDestinationPort()).toString());
+                } else if (entry.getProtocol() == UDP) {
+                    // Display UDP specific info, nothing really there
+                    UDPPacket packet = (UDPPacket) entry.getPacket().getPacket(Protocol.UDP);
+                    view.displayProtocol(UDP);
+                    view.displaySrc(packet.getSourceIP());
+                    view.displayDst(packet.getDestinationIP());
+                    view.displaySrcPort(new Integer(packet.getSourcePort()).toString());
+                    view.displayDstPort(new Integer(packet.getDestinationPort()).toString());
             } else {
                 // TODO implement more protocools
                 // Unsupported packet type handling
@@ -46,8 +60,26 @@ public class PacketInfoPresenter {
         }
     }
 
+    private String[] displayFlags(TCPPacket packet) {
+        String[] flags = new String[8];
+        flags[0] = (packet.isACK()) ? "ACK" : "";
+        flags[1] = (packet.isCWR()) ? "CWR" : "";
+        flags[2] = (packet.isECE()) ? "ECE" : "";
+        flags[3] = (packet.isFIN()) ? "FIN" : "";
+        flags[4] = (packet.isPSH()) ? "PSH" : "";
+        flags[5] = (packet.isRST()) ? "RST" : "";
+        flags[6] = (packet.isSYN()) ? "SYN" : "";
+        flags[7] = (packet.isURG()) ? "URG" : "";
+        return flags;
+    }
+
     public interface View {
+        void displayArrivalTime(String time);
         void displaySrc(String src);
-        void displayDst(String src);
+        void displaySrcPort(String port);
+        void displayDst(String dst);
+        void displayDstPort(String port);
+        void displayProtocol(String protocol);
+        void displayTcp(String ack, String seq, String[] flags);
     }
 }
--- app/src/main/java/com/example/packetsniffer/Views/PacketInfoActivity.java
+++ app/src/main/java/com/example/packetsniffer/Views/PacketInfoActivity.java
@@ -2,12 +2,15 @@ package com.example.packetsniffer.Views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.packetsniffer.Presenters.PacketInfoPresenter;
 import com.example.packetsniffer.R;
+
+import java.util.Arrays;
 
 public class PacketInfoActivity extends AppCompatActivity implements PacketInfoPresenter.View {
 
@@ -29,14 +32,57 @@ public class PacketInfoActivity extends AppCompatActivity implements PacketInfoP
     }
 
     @Override
+    public void displayArrivalTime(String time) {
+        TextView tvTime = (TextView) findViewById(R.id.textViewInfoArrival);
+        tvTime.setText(time);
+    }
+
+    @Override
     public void displaySrc(String src) {
         TextView tvSrc = (TextView) findViewById(R.id.textViewInfoSrc);
         tvSrc.setText(src);
     }
 
     @Override
-    public void displayDst(String dst) {
-        TextView tvDst = (TextView) findViewById(R.id.textViewInfoDest1);
-        tvDst.setText(dst);
+    public void displaySrcPort(String port) {
+        TextView tvSrcPort = (TextView) findViewById(R.id.textViewInfoSrcPort);
+        tvSrcPort.setText(port);
+    }
+
+    @Override
+    public void displayDst(String port) {
+        TextView tvDst = (TextView) findViewById(R.id.textViewInfoDest);
+        tvDst.setText(port);
+    }
+
+    @Override
+    public void displayDstPort(String port) {
+        TextView tvDstPort = (TextView) findViewById(R.id.textViewInfoDestPort);
+        tvDstPort.setText(port);
+    }
+
+    @Override
+    public void displayProtocol(String protocol) {
+        TextView tvProtocol = (TextView) findViewById(R.id.textViewInfoProtocol);
+        tvProtocol.setText(protocol);
+    }
+
+    @Override
+    public void displayTcp(String ack, String seq, String[] flags) {
+        View rowAck = findViewById(R.id.tcpRowAck);
+        View rowSeq = findViewById(R.id.tcpRowSeq);
+        View rowFlags = findViewById(R.id.tcpRowFlags);
+
+        TextView tvAck = (TextView) findViewById(R.id.textViewInfoTcpAck);
+        TextView tvSeq = (TextView) findViewById(R.id.textViewInfoTcpSeq);
+        TextView tvFlags = (TextView) findViewById(R.id.textViewInfoTcpFlags);
+
+        rowAck.setVisibility(View.VISIBLE);
+        rowSeq.setVisibility(View.VISIBLE);
+        rowFlags.setVisibility(View.VISIBLE);
+
+        tvAck.setText(ack);
+        tvSeq.setText(seq);
+        tvFlags.setText(Arrays.toString(flags));
     }
 }
