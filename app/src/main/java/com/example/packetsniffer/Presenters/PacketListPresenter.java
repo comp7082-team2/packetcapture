918b5f615fec388ceb404583dae3672717cc5295
maennis <mark.andrew.ennis@gmail.com>
2021-11-16 10:01:53 -0800

    Source and Destination filtering working

--- app/src/main/java/com/example/packetsniffer/Presenters/PacketListPresenter.java
+++ app/src/main/java/com/example/packetsniffer/Presenters/PacketListPresenter.java
@@ -18,15 +18,29 @@ public class PacketListPresenter {
     }
 
     public boolean isValidFilter(String expression) {
-        return expression.equals("tcp") || expression.equals("udp");
+        //return expression.equals("tcp") || expression.equals("udp");
+        return true;
     }
 
     public List<PcapEntry> loadFilteredPcap(String expression) {
         PcapFilter filter = new PcapFilter();
         String[] expressionArr = expression.split(" && ");
         for (String exp : expressionArr) {
+            exp = exp.trim();
             if (exp.equals("tcp") || exp.equals("udp")) {
                 filter.setProtocol(exp);
+            }
+            if (exp.startsWith("src ")) {
+                filter.setSrc(exp.replace("src ", "").trim());
+            }
+            if (exp.startsWith("dst ")) {
+                filter.setDst(exp.replace("dst ", "").trim());
+            }
+            if (exp.startsWith("srcPort ")) {
+                filter.setSrcPort(exp.replace("srcPort ", "").trim());
+            }
+            if (exp.trim().startsWith("dstPort ")) {
+                filter.setDstPort(exp.replace("dstPort ", "").trim());
             }
         }
         return pcapRepository.getEntries(filter);
