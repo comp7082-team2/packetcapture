df2b0a38b9f5d332c320c3edda87edb04489e108
cathy-bcit <ychan108@my.bcit.ca>
2021-11-15 22:28:24 -0800

    Added graph activity and view
    Co-authored-by: orangeappless <orangeappless@users.noreply.github.com>

---
+++ app/src/main/java/com/example/packetsniffer/Views/GraphActivity.java
@@ -0,0 +1,18 @@
+package com.example.packetsniffer.Presenters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
+public class GraphPresenter {
+
+}
