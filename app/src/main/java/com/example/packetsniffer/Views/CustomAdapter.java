c25b980030233d9c6269b449e605eb55b85761f5
Kshitiz Bisht <kshtiz123@gmail.com>
2021-11-16 10:41:46 -0800

    View Improved

--- app/src/main/java/com/example/packetsniffer/Views/CustomAdapter.java
+++ app/src/main/java/com/example/packetsniffer/Views/CustomAdapter.java
@@ -12,7 +12,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.packetsniffer.Models.PcapEntry;
 import com.example.packetsniffer.R;
 
+import java.io.IOException;
 import java.util.List;
+
+import io.pkts.packet.TCPPacket;
+import io.pkts.protocol.Protocol;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
 
@@ -24,17 +28,27 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
+        private final TextView textView2;
+        private final TextView textView3;
+
 
         public ViewHolder(View view) {
             super(view);
             // TODO: Set onClick listener to open detail view of packet
 
             textView = (TextView) view.findViewById(R.id.textView);
+            textView2 = (TextView) view.findViewById(R.id.src_adr);
+            textView3 = (TextView) view.findViewById(R.id.dst_adr);
         }
 
         public TextView getTextView() {
             return textView;
         }
+
+        public TextView getSrcAdr() {return textView2;}
+
+        public TextView getDstAdr() {return textView3;}
+
     }
 
     /**
@@ -51,14 +65,25 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.text_row_item, viewGroup, false);
-
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        TCPPacket packet = null;
         PcapEntry entry = localDataSet.get(position);
+        String src_adr = null, dst_adr = null;
+        try {
+            packet = (TCPPacket) entry.getPacket().getPacket(Protocol.TCP);
+            src_adr = packet.getSourceIP();
+            dst_adr = packet.getDestinationIP();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(entry.getArrivalTime() + " " + entry.getProtocol() + " " + src_adr + " " + dst_adr);
         String lineText = entry.getArrivalTime() + " " + entry.getProtocol();
+        viewHolder.getSrcAdr().setText(src_adr);
+        viewHolder.getDstAdr().setText(dst_adr);
         viewHolder.getTextView().setText(lineText);
         viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
