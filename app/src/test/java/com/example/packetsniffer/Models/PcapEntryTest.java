015580678c88000357fa7bebcde5e604ecc2a064
Dhruv Gandhi <dhruv.rai321@gmail.com>
2021-11-23 12:39:13 -0800

    created PcapEntry unit tests

---
+++ app/src/test/java/com/example/packetsniffer/Models/PcapEntryTest.java
@@ -0,0 +1,37 @@
+package com.example.packetsniffer.Models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PcapEntryTest {
+    PcapEntry pcapEntry = new PcapEntry("TCP", 1000000, null);
+
+    @Test
+    public void getProtocol() {
+        assertEquals("TCP", pcapEntry.getProtocol());
+    }
+
+    @Test
+    public void setProtocol() {
+        pcapEntry.setProtocol("UDP");
+        assertEquals("UDP", pcapEntry.getProtocol());
+    }
+
+    @Test
+    public void getArrivalTime() {
+        assertEquals(1000000, pcapEntry.getArrivalTime());
+    }
+
+    @Test
+    public void setArrivalTime() {
+        pcapEntry.setArrivalTime(10);
+        assertEquals(10, pcapEntry.getArrivalTime());
+    }
+
+    @Test
+    public void getPacket() {
+        assertNull(pcapEntry.getPacket());
+    }
+}
